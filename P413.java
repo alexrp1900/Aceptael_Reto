5f46c587b2787090e2a5d5cc58844c2cbeaddfdd
Alex <zeuslanzarayos@gmail.com>
2020-11-18 09:37:20 +0100

    h

---
+++ P413.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class P413 {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Dime un numero de losas claras");
+		int claras = teclado.nextInt();
+		System.out.println("Dime un numero de losas oscuras");
+		int oscuras = teclado.nextInt();
+		
+		while() {
+			
+		}
+				
+
+	}
+
+}
